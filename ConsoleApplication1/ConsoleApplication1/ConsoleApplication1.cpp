fe07989e03fe4c6ce3a4016d2295b93d009ea735
Михайло Бугера <mihapvp1990@gmail.com>
2025-04-22 19:59:30 +0300

    realise func cout

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,6 +13,8 @@ int main()
 
     list.print();
 
-    cout << list.rozm()<<endl;
-    cout << list.search(15)<<endl;
+    cout << list.rozm() << endl;
+    cout << list.search(15) << endl;
+    cout << list;
+}
     // для того щоб перегрузити оператор мені потрібно витянути прінт функ і запихнути і тіло перегрузки і порушувати принципиооп використовуючи френд
