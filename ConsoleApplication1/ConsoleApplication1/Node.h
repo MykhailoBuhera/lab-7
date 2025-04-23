8369b00640a221d888fbd9562b47af059cc86a84
Михайло Бугера <mihapvp1990@gmail.com>
2025-04-23 09:03:39 +0300

    u[date node for double linked list

--- ConsoleApplication1/ConsoleApplication1/Node.h
+++ ConsoleApplication1/ConsoleApplication1/Node.h
@@ -7,7 +7,7 @@ template <typename Type>
 struct Node {
     Type data; // Змінна, в якій записується значення.
     shared_ptr<Node<Type>> next; // Посилання на наступний елемент в листі.
-    shared_ptr<Node<Type>> previous;
+    shared_ptr<Node<Type>> prev;
 
-    Node(Type value) : data(value), next(nullptr), previous(nullptr) {}
+    Node(Type value) : data(value), next(nullptr), prev(nullptr) {}
 };
