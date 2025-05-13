e68c4557b180105e86a33edcffa1a336ffce87f4
Михайло Бугера <mihapvp1990@gmail.com>
2025-05-13 20:37:35 +0300

    end with stack

---
+++ ConsoleApplication1/ConsoleApplication1/stack.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include "Node.h"
+
+template <typename Type>
+class Stack
+{
+private:
+    shared_ptr<Node<Type>> data;
+
+public:
+
+    void push(Type value) { //add object in srat
+        auto new_node = make_shared<Node<Type>>(value); // Створюється новий об'єкт, якому назначається передана змінна value.
+        if (!data) {
+            data = new_node;
+            return;
+        }
+        new_node->next = data;// Вказівник next новоствореного об'єкту new_node вказує на попередньо створену змінну data(Адресу).                                     
+        data->previous = new_node;
+        data = new_node;
+    }
+
+    void isEmpty() {
+        if (!data) {
+            cout << "Stack is empty!" << endl;
+            return;
+        }
+    }
+
+    void pop() {//del strt element
+        if (!data) {
+            isEmpty();
+            return;
+        }
+        data = data->next;
+    }
+
+
+    void peek() { // displayed verhniy element
+        if (!data) {
+            isEmpty();
+            return;
+        }
+        cout << "Current element in the stack: " << data->data << endl;
+    }
+};
