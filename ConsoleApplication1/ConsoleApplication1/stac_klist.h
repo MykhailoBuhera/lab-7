0607eaeeddbdbafd74190af83c9889872198d1d0
Михайло Бугера <mihapvp1990@gmail.com>
2025-05-15 19:40:31 +0300

    add func pop and push

--- ConsoleApplication1/ConsoleApplication1/stac_klist.h
+++ ConsoleApplication1/ConsoleApplication1/stac_klist.h
@@ -2,4 +2,41 @@
 #include <iostream>
 #include <memory>
 #include "List.h"
+using namespace std;
+template <typename Type>
+class Stack {
+private:
+    list<Type> date;
 
+public:
+    void push(Type value) {
+        date.push_front(value); // Додаємо елемент на початок списку
+    }
+
+    void pop() {
+        if (container.empty()) {
+            cout << "Stack is empty! Cannot pop." << endl;
+            return;
+        }
+        date.pop_front(); // Видаляємо перший елемент списку
+    }
+
+    Type peek() {
+        if (container.empty()) {
+            cout << "Stack is empty"<<endl;
+        }
+        return date.ind(0); // Повертаємо перший елемент списку
+    }
+
+    bool empty() {
+        return data.empty();
+    }
+
+    int size() {
+        return container.rozm();
+    }
+
+    void print() const {
+        container.print();
+    }
+};
